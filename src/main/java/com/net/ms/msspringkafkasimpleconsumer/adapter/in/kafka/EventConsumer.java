2ca5edbda2adc7f5ff61f20ca2a4c4ae7f3e3585
joaoAlbertinoNeto <j.albertino.neto@gmail.com>
2023-04-13 20:52:02 -0300

    fixes and new adjust

--- src/main/java/com/net/ms/msspringkafkasimpleconsumer/adapter/in/kafka/EventConsumer.java
+++ src/main/java/com/net/ms/msspringkafkasimpleconsumer/adapter/in/kafka/EventConsumer.java
@@ -1,9 +1,9 @@
 package com.net.ms.msspringkafkasimpleconsumer.adapter.in.kafka;
 
+
+
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-
-import java.util.UUID;
 
 @Component
 public class EventConsumer {
